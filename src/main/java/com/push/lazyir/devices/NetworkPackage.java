0f5d963aeb65677c720973b9921837bca9912721
buhalo <ashparenij2@gmail.com>
2017-10-15 23:21:46 +0300

    Start work for cache in networkPackage

--- src/main/java/com/push/lazyir/devices/NetworkPackage.java
+++ src/main/java/com/push/lazyir/devices/NetworkPackage.java
@@ -8,16 +8,12 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.push.lazyir.Loggout;
-import com.push.lazyir.pojo.Command;
-import com.push.lazyir.pojo.CommandsList;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-import java.security.SecureRandom;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Created by buhalo on 05.03.17.
@@ -30,7 +26,12 @@ public class NetworkPackage {
     public final static String TYPE = "type";
     public final static String DATA = "data";
     public final static String N_OBJECT = "object";
+    public final static String DEVICE_TYPE = "deviceType";
 
+
+    //todo in Android version
+    //This variable depends on app version(pc or android)
+    public final static String DEVICE = "pc";
 
     private Device dv;
 
@@ -55,6 +56,7 @@ public class NetworkPackage {
         idNode.put(NAME,getMyName());
         idNode.put(TYPE,type);
         idNode.put(DATA,data);
+        idNode.put(DEVICE_TYPE,DEVICE);
     }
 
     public NetworkPackage(String message)
@@ -87,6 +89,11 @@ public class NetworkPackage {
       return idNode.toString();
     }
 
+
+    @Override
+    public int hashCode() {
+        return msg != null ? msg.hashCode() : 0;
+    }
 
     public String getType()
     {
@@ -178,4 +185,94 @@ public class NetworkPackage {
     }
 
 
+
+    // caching for intensibly usable networkPackets
+    public static class Cacher
+    {
+        //main Container for cache
+        private static ConcurrentHashMap<Integer,NetworkPackage> networkPackageCache = new ConcurrentHashMap<>();
+        // counter of most usable NetworkPackages by their hash key=hash,value=numberOfUsage
+        private static ConcurrentHashMap<Integer,Integer> usableCounter = new ConcurrentHashMap<>();
+
+        public Cacher() { }
+
+        public static void addToCache(Integer key,NetworkPackage networkPackage)
+        {
+            networkPackageCache.put(key,networkPackage);
+            clearIfNeeded();
+        }
+
+        //if in cache more than 20 items clear less useful items
+        //todo test this method carefully
+        private static void clearIfNeeded() {
+            if(networkPackageCache.size() > 20)
+            {
+                int lessValue = 10000;
+                int hashForRemove = -1;
+                for (Map.Entry<Integer, Integer> integerIntegerEntry : usableCounter.entrySet()) {
+                    Integer value = integerIntegerEntry.getValue();
+                    if(lessValue > value)
+                    {
+                        lessValue = value;
+                        hashForRemove = integerIntegerEntry.getKey();
+                    }
+                }
+                networkPackageCache.remove(hashForRemove);
+                usableCounter.remove(hashForRemove);
+
+            }
+        }
+
+        private static NetworkPackage getFromCache(Integer hash)
+        {
+            return networkPackageCache.get(hash);
+        }
+
+        //try get from cache, if null create and return new Object, increase counter by one, and add to cache if number ~10
+        public static NetworkPackage getOrCreatePackage(String type, String data)
+        {
+            //todo test this hairy hash method
+            int hash = type.hashCode() | data.hashCode();
+
+            NetworkPackage result = getFromCache(hash);
+            if(result == null) {
+                 result = createNewNetworkPackage(type, data);
+                countCacheAndAdd(hash,result);
+            }
+            return result;
+        }
+        //similar to other method, but for parsing answer message
+        public static NetworkPackage getOrCreatePackage(String message)
+        {
+            int hash = message.hashCode();
+            NetworkPackage result = getFromCache(hash);
+            if(result == null) {
+                result = createNewNetworkPackage(message);
+                countCacheAndAdd(hash,result);
+            }
+            return result;
+        }
+
+        private static void countCacheAndAdd(int hash,NetworkPackage np)
+        {
+            if(usableCounter.merge(hash,1,(value,one)->value + one) >= 10)
+                addToCache(hash,np);
+        }
+
+        private static NetworkPackage createNewNetworkPackage(String message)
+        {
+            return  new NetworkPackage(message);
+        }
+
+        private static NetworkPackage createNewNetworkPackage(String type, String data)
+        {
+            return  new NetworkPackage(type,data);
+        }
+
+
+
+
+
+
+    }
 }
